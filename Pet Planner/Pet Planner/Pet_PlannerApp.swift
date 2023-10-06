548f1a49ee81c016cb3602530a81a09a1f8884b5
Yusuf Kaan USTA <yusufkaanusta@192.168.1.49>
2023-10-06 09:47:28 +0300

    Test Commit

---
+++ Pet Planner/Pet Planner/Pet_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pet_PlannerApp.swift
+//  Pet Planner
+//
+//  Created by Yusuf Kaan USTA on 6.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Pet_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
